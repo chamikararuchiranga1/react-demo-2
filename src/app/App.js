20a10298fbeaeed9853f1d683af5744b50a84401
chami <chamikararuchiranga1@gmail.com>
2023-03-21 21:33:17 +0530

    create sid bar

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "../commen/component/Navbar/Navbar";
+import Sidebar from "../commen/component/Sidebar/Sidebar";
 
 function App() {
   return (
     <div>
         <Navbar/>
+        <Sidebar/>
     </div>
   );
 }
